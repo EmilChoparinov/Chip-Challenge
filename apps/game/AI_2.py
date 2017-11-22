7efc99168fab30e6c02b4c2d58e2d6a498fe739b
ale <alewolf@gmail.com>
2017-11-22 09:48:34 -0800

    progress in AI_2

--- apps/game/AI_2.py
+++ apps/game/AI_2.py
@@ -20,6 +20,25 @@ world=np.array([[5,5,5,5,5,5,5,5,5,5,5,5,5,5,5],
 [5,1,1,1,1,1,1,1,1,1,1,1,1,1,5], 
 [5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]])
 
+def distance(p1,p2, start):   
+    p1 = [p1['x'], p1['y']]
+    p2 = [p2['x'], p2['y']]
+    dist = cdist([p1,p2],[start],'cityblock')
+    return dist
+
+def get_walls(world):
+    """ from world gird get wall cells"""
+    wall = []
+    for x in range(world.shape[0]):
+        for y in range(world.shape[1]):
+            #print world[x][y]
+            if (world[x][y] == 3 or world[x][y]==5):
+                print world[x][y]
+                wall.append((x,y))
+    return tuple(wall)
+print get_walls(world)
+
+
 
 class Cell(object):
 
@@ -40,17 +59,54 @@ class Cell(object):
         self.f = 0
 
 
-def get_walls(world):
-    wall = []
-    for x in range(world.shape[0]):
-        for y in range(world.shape[1]):
-            #print world[x][y]
-            if (world[x][y] == 3 or world[x][y]==5):
-                print world[x][y]
-                wall.append((x,y))
-    return tuple(wall)
+class AStar(object):
+    def __init__(self, world):
+        self.opened = []
+        heapq.heapify(self.opened)
+        self.closed = set()
+        self.cells = []
+        self.grid_height = world.shape[1] 
+        self.grid_width = world.shape[0]
 
-print get_walls(world)
+    def init_grid(self, world):
+        walls = get_walls(world)
+        for x in range(self.grid_width):
+            for y in range(self.grid_height):
+                if (x, y) in walls:
+                    reachable = False
+                else:
+                    reachable = True
+                    self.cells.append(Cell(x, y, reachable))
+        if not(self.start):
+            start=[randrange(3,12), randrange(3,12)]
+            while world[start[0],start[1]] == 3:
+                start=[randrange(3,12), randrange(3,12)]
+                self.start = self.get_cell(start[0], start[1])
+        dist = distance(p1,p2,start)
+        if dist[0]<dist[1]:
+            self.end = self.get_cell(p1[0],p1[1])
+        else:
+            self.end = self.get_cell(p2[0],p2[1])
+
+    def get_heuristic(self, cell):
+    """
+    Compute the heuristic value H for a cell: distance between this cell and the ending cell multiply by 10.
+    parameter cell
+    returns heuristic value H
+    """
+        return 10 * (abs(cell.x - self.end.x) + abs(cell.y - self.end.y))
+
+    def get_cell(self, x, y):
+    """
+    Returns a cell from the cells list
+    @param x cell x coordinate
+    @param y cell y coordinate
+    @returns cell
+    """
+        return self.cells[x * self.grid_height + y]
+
+
+
 
 
 
