23af4b8189530227922422b5944fb4d37d774f94
Connor <connorzimmerman799@yahoo.com>
2017-11-21 16:25:20 -0800

    Control Start

--- apps/game/static/game/js/app.js
+++ apps/game/static/game/js/app.js
@@ -1,4 +1,35 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var world_render = new world()
     world_render.render()
+    var player = world_render.player;
 });
+
+$(document).on('keydown', 'body', function (e) {
+    switch (e.keycode) {
+        case 37:
+            if (world_render.playerPos["row"] - 1) {
+                break;
+            }
+            world_render.MoveRowBy(-1);
+            break;
+        case 38:
+            if (world_render.playerPos["row"] - 1) {
+                break;
+            }
+            world_render.MoveColBy(1);
+            break;
+        case 39:
+            if (world_render.playerPos["row"] - 1) {
+                break;
+            }
+            world_render.MoveRowBy(1);
+            break;
+        case 40:
+            if (world_render.playerPos["row"] - 1) {
+                break;
+            }
+            world_render.MoveColBy(-1);
+            break;
+    }
+})
+
